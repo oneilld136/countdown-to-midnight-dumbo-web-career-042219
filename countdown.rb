339421412bf87d4585ac4c51c1c863e32d810f33
oneilld136 <oneilld136@gmail.com>
2019-04-20 17:19:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,8 @@
-#write your code here
-
-def countdown
-end
+def countdown(number)
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end#write your code here
